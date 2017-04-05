870170e70f6e8656f8eaa7fa80e4f0b17a66469d
guojia <dmcguojia@qq.com>
2017-04-05 11:18:27 +0800

    新建项目

---
+++ portal/portal-query-statistics/src/main/java/com/zcbspay/platform/portal/query/statistics/App.java
@@ -0,0 +1,13 @@
+package com.zcbspay.platform.portal.query.statistics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ portal/portal-system-simple/src/main/java/com/zcbspay/platform/portal/system/simple/App.java
@@ -0,0 +1,13 @@
+package com.zcbspay.platform.portal.system.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
