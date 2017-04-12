ab82b84cf50c2ed3632cec61d945056902126388
guojia <dmcguojia@qq.com>
2017-04-12 16:02:31 +0800

    补充提交

---
+++ portal/portal-sms/src/main/java/com/zcbspay/platform/portal/sms/App.java
@@ -0,0 +1,13 @@
+package com.zcbspay.platform.portal.sms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
