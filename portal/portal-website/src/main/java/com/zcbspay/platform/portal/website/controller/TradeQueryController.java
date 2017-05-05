3fd9073bdd9714e0c70acd4f853dbd007210b5e2
bema <bema@DESKTOP-NTQ29PF>
2017-05-05 10:11:20 +0800

    删除无效引用

--- portal/portal-website/src/main/java/com/zcbspay/platform/portal/website/controller/TradeQueryController.java
+++ portal/portal-website/src/main/java/com/zcbspay/platform/portal/website/controller/TradeQueryController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.zcbspay.platform.manager.trade.service.TradeService;
 import com.zcbspay.platform.portal.query.statistics.bean.TxnsForPortalBean;
 import com.zcbspay.platform.portal.query.statistics.service.QueryAndStatService;
 /**
